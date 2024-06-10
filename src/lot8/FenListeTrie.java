4bf066c29fc978dc6691b002a5cdefff5dfca034
Fauchet <benjamin.fauchet@etudiant.univ-rennes1.fr>
2024-06-10 13:58:15 +0200

    fxml

---
+++ src/lot8/FenListeTrie.java
@@ -0,0 +1,46 @@
+package Fentetre;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class FenListeTrie extends Application{
+	static private Stage f;
+	public void start(Stage maFenetre) throws IOException{
+		//
+		
+		
+		f = new Stage();
+		f.setTitle("Liste trie résa annuler");
+		f.setResizable(false);
+		f.initModality(Modality.APPLICATION_MODAL);
+		
+		//maFenetre.setTitle("Formu test");
+		//maFenetre.setWidth(800);
+		//maFenetre.setHeight(800);
+		//maFenetre.setResizable(false);
+		//maFenetre.initModality(Modality.APPLICATION_MODAL);
+		Scene laScene = new Scene(creerSceneGraph());
+		f.setScene(laScene);
+		f.show();
+		
+	}
+	private Pane creerSceneGraph() throws IOException {
+		FXMLLoader loader;
+		loader = new FXMLLoader(getClass().getResource("/PageListeTrie.fxml"));
+		Pane racine = loader.load();
+		return racine;
+	}
+	public static void main(String args[]) {
+		Application.launch();
+	}
+	static public void fermer() {
+		f.close();
+	}
+}
