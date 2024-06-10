7147686d3af87658e8085d46ba841a8c35b1314d
Fauchet <benjamin.fauchet@etudiant.univ-rennes1.fr>
2024-06-10 17:49:05 +0200

    fxml

--- src/controleur/MainSae.java
+++ src/controleur/MainSae.java
@@ -1,5 +1,6 @@
 package controlleur;
 
+import vue.FenCreaZ;
 import vue.FenCreaZone;
 import vue.FenListeTrie;
 import vue.FenListeZone;
@@ -12,6 +13,7 @@ public class MainSae extends Application {
 	static private FenListeTrie fListe;
 	static private FenListeZone fZone;
 	static private FenCreaZone fCreaZone;
+	static private FenCreaZ fCreaZ;
 
 
 
@@ -20,13 +22,15 @@ public class MainSae extends Application {
 		fListe = new FenListeTrie();
 		fZone = new FenListeZone();
 		fCreaZone = new FenCreaZone();
+		fCreaZ = new FenCreaZ();
 		//fNouvel = new FenNouvelEmploye();
 		//fModifier = new FenModifierEmploye();
 		//fNouvel.initModality(Modality.APPLICATION_MODAL);
 		//fModifier.initModality(Modality.APPLICATION_MODAL);
 		//fListe.initModality(Modality.APPLICATION_MODAL);
 		//fListe.show();
-		fCreaZone.show();
+		//fCreaZone.show();
+		fCreaZ.show();
 	}
 	public static void main(String args[]) {
 		Application.launch();
